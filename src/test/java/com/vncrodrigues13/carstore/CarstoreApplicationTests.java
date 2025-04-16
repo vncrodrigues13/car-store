81eaf53f81d3cddaff2fd31de5a2f5d5fb5beb52
vncrodrigues13 <viniciusalmeidarodrigues96@gmail.com>
2025-04-16 09:10:22 -0300

    import project from sprint initializr

---
+++ src/test/java/com/vncrodrigues13/carstore/CarstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vncrodrigues13.carstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
